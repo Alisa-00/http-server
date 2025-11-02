e8874fa200c6bf5e306dcc48dc0b3d1b6045efc3
Alisa <altinao@pm.me>
2025-11-02 10:13:10 +0200

    removes unused test in main

--- src/main.zig
+++ src/main.zig
@@ -9,12 +9,3 @@ pub fn main() !void {
     try stdout.print("hello this is a test\n", .{});
     try stdout.flush();
 }
-
-test "simple test" {
-    //try parse.parseRequest();
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
