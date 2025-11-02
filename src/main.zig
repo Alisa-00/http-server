f618d4b6e2c3415cc242994e6ae576047152c12f
Alisa <altinao@pm.me>
2025-11-02 12:01:05 +0200

    removes different module for parse.zig and instead implements everything through main.zig. adds parse example in main, as well as calls test modules

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,27 @@
 const std = @import("std");
-const parse = @import("parse");
+const parse = @import("parse.zig");
+const http = @import("http.zig");
 
 pub fn main() !void {
     var out_buffer: [1024]u8 = undefined;
     var out_writer = std.fs.File.stdout().writer(&out_buffer);
     const stdout = &out_writer.interface;
 
-    try stdout.print("hello this is a test\n", .{});
+    const allocator = std.heap.page_allocator;
+
+    const REQUEST = "GET / HTTP/1.1\n";
+
+    var method_map = try http.initMethodMap(allocator);
+    defer method_map.deinit();
+    var version_map = try http.initVersionMap(allocator);
+    defer version_map.deinit();
+
+    const parsed = try parse.parseRequest(REQUEST, method_map, version_map);
+
+    try stdout.print("hello this is a test:\n{s}\n{s}\n{s}\n", .{ @tagName(parsed.method), parsed.path, @tagName(parsed.version) });
     try stdout.flush();
+}
+
+test {
+    _ = @import("parse.zig");
 }
